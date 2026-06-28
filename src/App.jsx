1170b7a69c320bc9603e296d8c5d98e06c46589d
DipakGupta2006 <idipakgupta2006@gmail.com>
2026-06-28 16:19:07 +0530

    basic done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [task, setTask] = useState([]);
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+
+  const handleTitle = ((e) => {
+    setTitle(e.target.value);
+  });
+
+  const handleDesc = ((e) => {
+    setDesc(e.target.value);
+  });
+
+  const addTask = ((e) => {
+    e.preventDefault();
+
+    const data = {
+      title: title,
+      desc: desc
+    };
+
+    setTask([...task, data]);
+    setTitle("");
+    setDesc("");
+  });
+
+
   return (
     <div>
-      hello
-      
+
+
+      <form>
+
+        <p>Enter Task Name: </p>
+        <input type="text" name="title" value={title} id="" onChange={handleTitle} />
+
+        <br /><br />
+
+        <p>Enter Description: </p>
+        <input type="text" name="desc" value={desc} id="" onChange={handleDesc} />
+
+        <br /><br />
+
+        <button onClick={addTask}>Add Task</button>
+      </form>
+
+
+
+
+      <ul>
+        {task.map((data, index) => (
+          <li key={index}>
+            <h3>{data.title}</h3>
+
+            <p>
+               {data.desc}
+            </p>
+
+            {/* <button onClick={() => editProfile(index)}>Edit</button> */}
+          </li>
+        ))}
+      </ul>
+
+
+
     </div>
   )
 }
 
-export default App
+export default App;
